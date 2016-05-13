5201a984d6befebba00f3b5710d629c8f1d3e298
l2015y <l2015y@yandex.ua>
2016-05-13 16:22:03 +0300

    add whisper

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-var people = {};
+var users = {};
 
 
 app.get('/', function(req,res){
     var file = new fs.ReadStream('index.html');
     sendFile(file,res);
 });
+
 function sendFile(file,res){
     file.pipe(res);
     file.on('error',function(err){
@@ -20,35 +21,62 @@ function sendFile(file,res){
         file.destroy();
     });
 };
+
    io.on('connection',function(socket) {
        console.log('a user connected');
-       socket.on('join',function(name){
-               people[socket.id] = name;
-               console.log(people);
-               socket.broadcast.emit("join", name);
-               socket.emit("join", name);
-               socket.emit("notice", "You have connected to the server.");
-               socket.broadcast.emit("notice", name + " has joined the server.");
+       socket.on('join',function(data,callback) {
+           if (data in users) {
+               callback(false);
+           } else {
+               callback(true);
+               socket.nickname = data;
+               users[socket.nickname] = socket;
+               updateNickname();
+           }
+       });
 
-       });
+       function updateNickname(){
+           io.sockets.emit('usernames', Object.keys(users));
+       }
+
+
         socket.on('is typing',function(data){
-           socket.broadcast.emit("typing",people[socket.id] + ' is typing a message now ');
+           socket.broadcast.emit("typing",socket.nickname + ' is typing a message now ');
        });
-       socket.on('chat message', function (msg) {
-           socket.broadcast.emit("chat message",people[socket.id], msg);
-           socket.emit("chat message",people[socket.id], msg);
+
+       socket.on('chat message', function (data,callback) {
+           var msg = data.trim();
+           if(msg.substr(0,3) === '/w '){
+               msg = msg.substr(3);
+               var ind = msg.indexOf(' ');
+               if(ind !== -1){
+                   var name = msg.substring(0, ind);
+                   var msg = msg.substring(ind + 1);
+                   if(name in users){
+                       users[name].emit('whisper', socket.nickname, msg);
+                   } else{
+                       callback('Error!  Enter a valid user.');
+                   }
+               } else{
+                   callback('Error!  Please enter a message for your whisper.');
+               }
+           } else {
+               socket.broadcast.emit("chat message", socket.nickname, msg);
+               socket.emit("chat message", socket.nickname, msg);
+           }
        });
-       socket.on("disconnect", function () {
-           socket.broadcast.emit('notice', people[socket.id] + ' has left the chat.');
-           delete people[socket.id];
-           console.log('user disconnected');
+
+       socket.on('disconnect', function(data){
+           if(!socket.nickname) return;
+           delete users[socket.nickname];
+           updateNickname();
        });
 
        });
 http.listen(3000, function () {
     console.log('listening *:3000')
 });
-global.people = people;
+
 
 
 
